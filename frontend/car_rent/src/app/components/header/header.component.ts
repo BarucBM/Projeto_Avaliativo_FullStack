4bd9debe568abc7d71979cb0497e04e9cc6bd0b5
alex cesar <thealexcesar@gmail.com>
2024-08-26 18:16:41 -0300

    Layout inicial

--- frontend/car_rent/src/app/components/header/header.component.ts
+++ frontend/car_rent/src/app/components/header/header.component.ts
@@ -1,31 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {Router} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'rent-header',
   standalone: true,
   imports: [
-    FaIconComponent
+    CommonModule,
+    FaIconComponent,
+    NavbarComponent
   ],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.sass'
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  dropdownOpen = false;
-  isOpen = false;
+  dropdownOpen: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
-  toggleDropdown() {
+  toggleDropdown(): void {
     this.dropdownOpen = !this.dropdownOpen;
   }
 
-  closeDropdown() {
+  logout(): void {
+    this.router.navigate(['/']).then((): void => this.closeDropdown());
+  }
+
+  protected closeDropdown(): void {
     this.dropdownOpen = false;
   }
 
-  logout() {
-    this.router.navigate(['/']);
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event): void {
+    const targetElement = event.target as HTMLElement;
+    if (!targetElement.closest('.relative') && this.dropdownOpen) {
+      this.closeDropdown();
+    }
   }
 }
