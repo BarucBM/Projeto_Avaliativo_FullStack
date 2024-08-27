0c4ffa3790600113eb574e1f003446ae5aa89ece
alex cesar <thealexcesar@gmail.com>
2024-08-27 08:51:28 -0300

    Estilizacao Veiculos + Dashboard

--- frontend/car_rent/src/app/components/vehicles/vehicles.component.ts
+++ frontend/car_rent/src/app/components/vehicles/vehicles.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
-  selector: 'rent-vehicles',
-  standalone: true,
-  imports: [],
-  templateUrl: './vehicles.component.html'
+    selector: 'vehicles',
+    standalone: true,
+    imports: [
+        FaIconComponent
+    ],
+    templateUrl: './vehicles.component.html'
 })
 export class VehiclesComponent {
-
+    vehicle = {
+        type: "Carro",
+        brand: "BMW",
+        model: "X6",
+        year: "2024",
+    }
 }
